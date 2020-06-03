212e3c5ae7461939738e58cf8c398aafe9a25dfc
KhairulIzwan <wansnap@gmail.com>
2020-06-03 15:32:10 +0800

    Updated on 06-03-2020-15:20:45

--- script/box_validity_simplified_i2c.py
+++ script/box_validity_simplified_i2c.py
@@ -74,7 +74,7 @@ class BoxIDValidate_node:
 			self.boxState.data = boxState
 			self.boxStatus_pub.publish(self.boxState)
 
-	def readI2C():
+	def readI2C(self):
 		inData = i2c.read_byte(I2C_ADD)
 		return inData
 
