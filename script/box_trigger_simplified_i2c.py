07500b2babcda2ebaba03ad13979710235dca1e6
khairulizwan <wansnap@gmail.com>
2020-06-03 16:09:43 +0800

    Updated on 06-03-2020-15:44:09 from 172.20.10.2 : khairulizwan-MacBookPro

---
+++ script/box_trigger_simplified_i2c.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+################################################################################
+## {Description}: Triggering the Relay
+################################################################################
+## Author: Khairul Izwan Bin Kamsani
+## Version: {1}.{0}.{0}
+## Email: {wansnap@gmail.com}
+################################################################################
+
+from __future__ import print_function
+from __future__ import division
+
+import sys
+import rospy
+
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from self_collect_machine.msg import boxStatus
+
+import csv
+import datetime
+import cv2
+
+import smbus
+
+i2c = smbus.SMBus(1)
+I2C_ADD = 0x09 # Arduino I2C address
+
+class BoxIDTrigger_node:
+	def __init__(self):
+
+		self.boxState = boxStatus()
+		self.prevI2CData = 0
+
+		# Initializing your ROS Node
+		rospy.init_node('box_validity', anonymous=False)
+
+		# Subscribe Int32 msg
+		self.boxID_activation_sub = rospy.Subscriber("/boxID_activation", Int32, self.cbBoxTrigger)
+
+	def cbBoxTrigger(self, msg):
+
+		i2c.write_byte(I2C_ADD, msg.data)
+		rospy.loginfo(msg.data)
+
+def main(args):
+
+	vn = BoxIDTrigger_node()
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		rospy.loginfo("[INFO] BoxIDTrigger_node [OFFLINE]...")
+
+if __name__ == '__main__':
+	main(sys.argv)
