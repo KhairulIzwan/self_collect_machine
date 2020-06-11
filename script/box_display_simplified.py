3811b277fbc2b88027ab68b03648816e93a76416
khairulizwan <wansnap@gmail.com>
2020-06-11 23:30:51 +0800

    Updated on 06-11-2020-19:11:58 from 172.20.10.2 : khairulizwan-MacBookPro

--- script/box_display_simplified.py
+++ script/box_display_simplified.py
@@ -139,16 +139,17 @@ class BoxIDDisplay:
 						font=proportional(CP437_FONT))
 				rospy.sleep(1)
 				self.cbBox()
-				if not self.box.data:
-					with canvas(self.virtual) as draw:
-						text(draw, (1, 1), "N/A", 
-							fill="white", font=proportional(CP437_FONT))
-					rospy.sleep(1)
-				else:
-					with canvas(self.virtual) as draw:
-						text(draw, (1, 1), "{}".format(self.box), 
-							fill="white", font=proportional(CP437_FONT))
-					rospy.sleep(1)
+#				if not self.box.data:
+#					with canvas(self.virtual) as draw:
+#						text(draw, (1, 1), "N/A", 
+#							fill="white", font=proportional(CP437_FONT))
+#					rospy.sleep(1)
+#				else:
+#					with canvas(self.virtual) as draw:
+#						text(draw, (1, 1), "{}".format(self.box), 
+#							fill="white", font=proportional(CP437_FONT))
+#					rospy.sleep(1)
+				rospy.loginfo(self.box.data)
 
 #			else:
 #				with canvas(self.virtual) as draw:
