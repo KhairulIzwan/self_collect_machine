8c564e7f188870fd7c5043e8362b8395ee359943
khairulizwan <wansnap@gmail.com>
2020-06-11 22:48:43 +0800

    Updated on 06-11-2020-19:11:58 from 172.20.10.2 : khairulizwan-MacBookPro

--- script/box_display_simplified.py
+++ script/box_display_simplified.py
@@ -34,8 +34,6 @@ class BoxIDDisplay:
 		self.device.contrast(5)
 		self.virtual = viewport(self.device, width=32, height=16)
 
-		rospy.init_node("boxID_display", anonymous=False)
-
 		self.sensor_received = False
 		self.code_received = False
 		self.box_received = False
@@ -96,57 +94,55 @@ class BoxIDDisplay:
 
 		while not rospy.is_shutdown():
 			self.cbQRmode()
-			rospy.loginfo(self.mode.data)
 
-#		if self.mode.data == "customer":
-#			ropsy.loginfo("CUSTOMER")
-#			with canvas(self.virtual) as draw:
-#				text(draw, (1, 1), "TRUE", fill="white", font=proportional(CP437_FONT))
-#				rospy.sleep(1)
-#			if self.mode == "customer":
-#				with canvas(self.virtual) as draw:
-#					text(draw, (1, 1), "CUST", fill="white", 
-#						font=proportional(CP437_FONT))
-#					rospy.sleep(2)
-#				if self.sensor_received:
-#					with canvas(self.virtual) as draw:
-#						text(draw, (1, 1), "{}".format(self.sensor), 
-#							fill="white", font=proportional(CP437_FONT))
-#					rospy.sleep(2)
-#					self.code_received = False
-#				else:
-#					with canvas(self.virtual) as draw:
-#						text(draw, (1, 1), "N/A", 
-#							fill="white", font=proportional(CP437_FONT))
-#					rospy.sleep(2)
-#			elif self.mode == "store":
-#				with canvas(self.virtual) as draw:
-#					text(draw, (1, 1), "STOR", fill="white", 
-#						font=proportional(CP437_FONT))
-#					rospy.sleep(2)
-#				if self.box_received:
-#					with canvas(self.virtual) as draw:
-#						text(draw, (1, 1), "{}".format(self.box), 
-#							fill="white", font=proportional(CP437_FONT))
-#					rospy.sleep(2)
-#					self.code_received = False
-#				else:
-#					with canvas(self.virtual) as draw:
-#						text(draw, (1, 1), "N/A", 
-#							fill="white", font=proportional(CP437_FONT))
-#					rospy.sleep(2)
-#		else:
-#			ropsy.loginfo("CUSTOMER X")
-#			with canvas(self.virtual) as draw:
-#				text(draw, (1, 1), "SCAN", fill="white", font=proportional(CP437_FONT))
-#				rospy.sleep(1)
-#			self.sensor_received = False
-#			self.box_received = False
-#			self.sensor = "N/A"
-#			self.box = "N/A"
+			if self.mode.data == "customer":
+				with canvas(self.virtual) as draw:
+					text(draw, (1, 1), "TRUE", fill="white", font=proportional(CP437_FONT))
+					rospy.sleep(1)
+	#			if self.mode == "customer":
+	#				with canvas(self.virtual) as draw:
+	#					text(draw, (1, 1), "CUST", fill="white", 
+	#						font=proportional(CP437_FONT))
+	#					rospy.sleep(2)
+	#				if self.sensor_received:
+	#					with canvas(self.virtual) as draw:
+	#						text(draw, (1, 1), "{}".format(self.sensor), 
+	#							fill="white", font=proportional(CP437_FONT))
+	#					rospy.sleep(2)
+	#					self.code_received = False
+	#				else:
+	#					with canvas(self.virtual) as draw:
+	#						text(draw, (1, 1), "N/A", 
+	#							fill="white", font=proportional(CP437_FONT))
+	#					rospy.sleep(2)
+	#			elif self.mode == "store":
+	#				with canvas(self.virtual) as draw:
+	#					text(draw, (1, 1), "STOR", fill="white", 
+	#						font=proportional(CP437_FONT))
+	#					rospy.sleep(2)
+	#				if self.box_received:
+	#					with canvas(self.virtual) as draw:
+	#						text(draw, (1, 1), "{}".format(self.box), 
+	#							fill="white", font=proportional(CP437_FONT))
+	#					rospy.sleep(2)
+	#					self.code_received = False
+	#				else:
+	#					with canvas(self.virtual) as draw:
+	#						text(draw, (1, 1), "N/A", 
+	#							fill="white", font=proportional(CP437_FONT))
+	#					rospy.sleep(2)
+			else:
+				with canvas(self.virtual) as draw:
+					text(draw, (1, 1), "SCAN", fill="white", font=proportional(CP437_FONT))
+					rospy.sleep(1)
+	#			self.sensor_received = False
+	#			self.box_received = False
+	#			self.sensor = "N/A"
+	#			self.box = "N/A"
 
 def main(args):
 	display = BoxIDDisplay()
+	rospy.init_node("boxID_display", anonymous=False)
 
 	try:
 		rospy.spin()
