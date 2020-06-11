246f67b69a85402c6bf44a3f52b7791d5c09b338
khairulizwan <wansnap@gmail.com>
2020-06-12 00:19:59 +0800

    Updated on 06-11-2020-19:11:58 from 172.20.10.2 : khairulizwan-MacBookPro

--- script/box_display_simplified.py
+++ script/box_display_simplified.py
@@ -34,59 +34,42 @@ class BoxIDDisplay:
 		self.device.contrast(5)
 		self.virtual = viewport(self.device, width=32, height=16)
 
-		rospy.init_node("boxID_display", anonymous=False)
-
 		rospy.on_shutdown(self.shutdown)
 
 		# Subscribe Int32 msg
 		self.sensor_topic = "/boxID_activation"
-#		self.sensor_sub = rospy.Subscriber(self.sensor_topic, Int32, self.cbID)
-		self.sensor_sub = rospy.Subscriber(self.sensor_topic, Int32)
+		self.sensor_sub = rospy.Subscriber(self.sensor_topic, Int32, self.cbID)
 
 		# Subscribe Int32 msg
 		self.box_topic = "/box_position"
-#		self.box_sub = rospy.Subscriber(self.box_topic, Int32, self.cbBox)
-		self.box_sub = rospy.Subscriber(self.box_topic, Int32)
+		self.box_sub = rospy.Subscriber(self.box_topic, Int32, self.cbBox)
 
 		# Subscribe String msg
 		self.mode_topic = "/scan_mode"
-#		self.mode_sub = rospy.Subscriber(self.mode_topic, String, self.cbQRmode)
-		self.mode_sub = rospy.Subscriber(self.mode_topic, String)
+		self.mode_sub = rospy.Subscriber(self.mode_topic, String, self.cbQRmode)
+
+	def cbID(self, msg):
+
+		try:
+			self.sensor = msg.data
+		except KeyboardInterrupt as e:
+			print(e)
+
+	def cbQRmode(self, msg):
+
+		try:
+			self.mode = msg.data
+		except KeyboardInterrupt as e:
+			print(e)
 
 		self.update_display()
 
-#	def cbID(self, msg):
-	def cbID(self):
+	def cbBox(self, msg):
 
-#		try:
-#			self.sensor = msg.data
-#		except KeyboardInterrupt as e:
-#			print(e)
-
-#		self.sensor_received = True
-		self.sensor = rospy.wait_for_message(self.sensor_topic, String)
-
-#	def cbQRmode(self, msg):
-	def cbQRmode(self):
-
-#		try:
-#			self.mode = msg.data
-#		except KeyboardInterrupt as e:
-#			print(e)
-
-#		self.code_received = True
-		self.mode = rospy.wait_for_message(self.mode_topic, String)
-
-#	def cbBox(self, msg):
-	def cbBox(self):
-
-#		try:
-#			self.box = msg.data
-#		except KeyboardInterrupt as e:
-#			print(e)
-
-#		self.box_received = True
-		self.box = rospy.wait_for_message(self.box_topic, String)
+		try:
+			self.box = msg.data
+		except KeyboardInterrupt as e:
+			print(e)
 
 	def shutdown(self):
 		try:
@@ -96,53 +79,43 @@ class BoxIDDisplay:
 
 	def update_display(self):
 
-		while not rospy.is_shutdown():
-			self.cbQRmode()
-#			self.cbID()
-#			self.cbBox()
-
-			if self.mode.data == "customer":
-				with canvas(self.virtual) as draw:
-					text(draw, (1, 1), "CUST", fill="white", 
-						font=proportional(CP437_FONT))
-				rospy.sleep(1)
-				self.cbID()
-				if not self.sensor.data:
-					with canvas(self.virtual) as draw:
-						text(draw, (1, 1), "N/A", 
-							fill="white", font=proportional(CP437_FONT))
-					rospy.sleep(5)
-				else:
-					with canvas(self.virtual) as draw:
-						text(draw, (1, 1), "{}".format(self.sensor.data), 
-							fill="white", font=proportional(CP437_FONT))
-					rospy.sleep(5)
-			elif self.mode.data == "store":
-				with canvas(self.virtual) as draw:
-					text(draw, (1, 1), "STOR", fill="white", 
-						font=proportional(CP437_FONT))
-				rospy.sleep(1)
-				self.cbBox()
-				if not self.box.data:
-					with canvas(self.virtual) as draw:
-						text(draw, (1, 1), "N/A", 
-							fill="white", font=proportional(CP437_FONT))
-					rospy.sleep(5)
-				else:
-					with canvas(self.virtual) as draw:
-						text(draw, (1, 1), "{}".format(self.box.data), 
-							fill="white", font=proportional(CP437_FONT))
-					rospy.sleep(5)
+		if self.mode == "customer":
+			with canvas(self.virtual) as draw:
+				text(draw, (1, 1), "CUST", fill="white", 
+					font=proportional(CP437_FONT))
+			rospy.sleep(1)
+#			if not self.sensor.data:
+#				with canvas(self.virtual) as draw:
+#					text(draw, (1, 1), "N/A", 
+#						fill="white", font=proportional(CP437_FONT))
+#				rospy.sleep(5)
 #			else:
 #				with canvas(self.virtual) as draw:
-#					text(draw, (1, 1), "SCAN", fill="white", font=proportional(CP437_FONT))
-	#			self.sensor_received = False
-	#			self.box_received = False
-	#			self.sensor = "N/A"
-	#			self.box = "N/A"
+#					text(draw, (1, 1), "{}".format(self.sensor.data), 
+#						fill="white", font=proportional(CP437_FONT))
+#				rospy.sleep(5)
+		elif self.mode == "store":
+			with canvas(self.virtual) as draw:
+				text(draw, (1, 1), "STOR", fill="white", 
+					font=proportional(CP437_FONT))
+			rospy.sleep(1)
+#			if not self.box.data:
+#				with canvas(self.virtual) as draw:
+#					text(draw, (1, 1), "N/A", 
+#						fill="white", font=proportional(CP437_FONT))
+#				rospy.sleep(5)
+#			else:
+#				with canvas(self.virtual) as draw:
+#					text(draw, (1, 1), "{}".format(self.box.data), 
+#						fill="white", font=proportional(CP437_FONT))
+#				rospy.sleep(5)
+		else:
+			with canvas(self.virtual) as draw:
+				text(draw, (1, 1), "SCAN", fill="white", font=proportional(CP437_FONT))
 
 def main(args):
 	display = BoxIDDisplay()
+	rospy.init_node("boxID_display", anonymous=False)
 
 	try:
 		rospy.spin()
